aede4ba22a19f9191fb79d0897c20b0197fb9ba1
quoctrung163 <quoctrung163@yandex.com>
2020-02-27 15:13:42 +0700

    done filter state

--- monster-rolodex/src/App.js
+++ monster-rolodex/src/App.js
@@ -1,30 +1,38 @@
-import React, { Component } from 'react';
-import { CardList } from './components/card-list/card-list.component';
+import React, {Component} from 'react';
+import {CardList} from './components/card-list/card-list.component';
 import './App.css';
 
 class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            monsters: []
-        };
-    }
+   constructor() {
+      super();
+      this.state = {
+         monsters: [],
+         searchField: ''
+      };
+   }
 
-    componentDidMount() {
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(res => res.json())
-            .then(users => this.setState({
-                monsters: users
-            }))
-    }
+   componentDidMount() {
+      fetch('https://jsonplaceholder.typicode.com/users')
+         .then(res => res.json())
+         .then(users => this.setState({monsters: users}))
+   }
 
-    render() {
-        return (
-            <div className="App">
-                <CardList monsters={this.state.monsters} />
-            </div>
-        );
-    }
+   render() {
+      const { monsters, searchField } = this.state;
+      const filteredMonsters = monsters.filter(monster => (
+         monster.name.toLowerCase().includes(searchField.toLowerCase())
+      ));
+
+      return (
+      <div className="App">
+         <input
+            type='search'
+            placeholder='search monster'
+            onChange={e => this.setState({searchField: e.target.value})}/>
+         <CardList monsters={filteredMonsters}/>
+      </div>
+      );
+   }
 }
 
 export default App;
